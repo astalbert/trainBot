01ae4e02179938428e54bd7ddc14b702ff446bdf
astalbert <57797266+astalbert@users.noreply.github.com>
2023-02-20 23:54:09 +0300

    Add files via upload

---
+++ trainBot/telegram/api.py
@@ -0,0 +1,39 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ContentType, ParseMode
+from aiogram.utils import executor
+from aiogram.utils.emoji import emojize
+from aiogram.utils.markdown import text, italic, code
+
+from common.config import BOT_TOKEN
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+@dp.message_handler(content_types=ContentType.ANY)
+async def unknown_message(msg: types.Message):
+    message_text = text(emojize('Я не знаю, что с этим делать :astonished:'),
+                        italic('\nЯ просто напомню,'), 'что есть',
+                        code('команда'), '/help')
+    await msg.reply(message_text, parse_mode=ParseMode.MARKDOWN)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
